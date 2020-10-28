be8ac8a57514506262ddc72c10e2f54aefdc94d1
the-eugene <72322861+the-eugene@users.noreply.github.com>
2020-10-27 19:27:19 -0500

    Eugene

--- app/src/main/java/com/example/festivalawardtracker/MainActivity.java
+++ app/src/main/java/com/example/festivalawardtracker/MainActivity.java
@@ -9,12 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //This is from James
-        //This is from cayla T
-        //Eugene
-        //This is my comment
-        // Carlos
-
         setContentView(R.layout.activity_main);
     }
 }
