b256d337e33992a02d547177d2b4967569256e76
CaylaT <72427853+caylaet@users.noreply.github.com>
2020-10-27 20:15:57 -0400

    Testing

--- app/src/main/java/com/example/festivalawardtracker/MainActivity.java
+++ app/src/main/java/com/example/festivalawardtracker/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //This is from cayla T
+        //This is my second comment
         //Eugene
         //This is my comment
         // Carlos
