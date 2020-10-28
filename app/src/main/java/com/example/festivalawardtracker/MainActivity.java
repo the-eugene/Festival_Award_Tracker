1349ac4dbac2709db79ee7a408f1280e85d4e8b1
CaylaT <72427853+caylaet@users.noreply.github.com>
2020-10-27 20:24:05 -0400

    Testing

--- app/src/main/java/com/example/festivalawardtracker/MainActivity.java
+++ app/src/main/java/com/example/festivalawardtracker/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //This is from James
-        //This is from cayla
+        //This is from cayla T
         //Eugene
         //This is my comment
         // Carlos
