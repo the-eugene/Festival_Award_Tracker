bd2dfbe7fd72f9378747c02151ad23399d371c7e
the-eugene <72322861+the-eugene@users.noreply.github.com>
2020-11-14 12:31:11 -0600

    Material Component Layout Test

--- app/src/main/java/com/example/festivalawardtracker/Instrument.java
+++ app/src/main/java/com/example/festivalawardtracker/Instrument.java
@@ -1,5 +1,17 @@
 package com.example.festivalawardtracker;
 
+import java.util.Arrays;
+
 public enum Instrument {
     piano, violin, viola, cello
+    ;
+
+    public static String[] Options() {
+        String[] options=new String[Instrument.values().length];
+        for (int i=0; i<Instrument.values().length; i++) {
+            options[i]=Instrument.values()[i].toString();
+            options[i]=options[i].substring(0, 1).toUpperCase() + options[i].substring(1);
+        }
+        return options;
+    }
 }
