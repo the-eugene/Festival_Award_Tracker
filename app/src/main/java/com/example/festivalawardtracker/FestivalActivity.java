e7547e5d0eb9ca43f958cec8d74e941c97e0174e
carloswm85 <carloswashingtonmercado@gmail.com>
2020-11-17 12:31:57 -0300

    Fixed Event Activity Toolbar.

--- app/src/main/java/com/example/festivalawardtracker/FestivalActivity.java
+++ app/src/main/java/com/example/festivalawardtracker/FestivalActivity.java
@@ -1,20 +1,33 @@
 package com.example.festivalawardtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
+/**
+ * @author carloswashingtonmercado@gmail.com
+ */
 public class FestivalActivity extends AppCompatActivity {
+
+    private Toolbar toolbarFestival;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_festival);
 
+        /* ACTION BAR */
+        toolbarFestival = findViewById(R.id.toolbarNewEvent);
+        toolbarFestival.setTitle("Add Festival");
+        setSupportActionBar(toolbarFestival);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         /* DROPDOWN LIST is NFMC festival? Yes or No question */
         String[] YESORNO = new String[] {"Yes", "No"};
+        // ADAPTER
         ArrayAdapter<String> adapterYesOrNO =
                 new ArrayAdapter<>(
                         this,
