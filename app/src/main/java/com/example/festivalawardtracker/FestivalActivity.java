d7bf8cee68702d60c253175210ee730bbe721b47
Jimmy <eng15005@byui.edu>
2020-11-25 19:08:06 -0800

    added database push event to FestivalActivity.

--- app/src/main/java/com/example/festivalawardtracker/FestivalActivity.java
+++ app/src/main/java/com/example/festivalawardtracker/FestivalActivity.java
@@ -5,22 +5,31 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 /**
- * @author carloswashingtonmercado@gmail.com
+ * Create new festival activity that adds festivals to the database
+ * @author carloswashingtonmercado@gmail.com and Jimmy
  */
 public class FestivalActivity extends AppCompatActivity {
 
     private Toolbar toolbarFestival;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_festival);
+        final EditText editFestivalName = (EditText)findViewById(R.id.editTextNewFestivalName);
+        final AutoCompleteTextView editIsNFMC = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextViewDropdownNewFestivalNFMC);
 
-        /* ACTION BAR */
+                /* ACTION BAR */
         toolbarFestival = findViewById(R.id.toolbarNewFestival);
         toolbarFestival.setTitle("Add Festival");
         toolbarFestival.setTitleTextColor(Color.WHITE);
@@ -38,6 +47,39 @@ public class FestivalActivity extends AppCompatActivity {
         AutoCompleteTextView editTextFilledExposedDropdownInstruments =
                 this.findViewById(R.id.autoCompleteTextViewDropdownNewFestivalNFMC);
         editTextFilledExposedDropdownInstruments.setAdapter(adapterYesOrNO);
+
+        //firebase set up
+        DBManager.setCurrentDB("Event");
+
+        MaterialButton addFestival = findViewById(R.id.btnSaveFestival);
+        addFestival.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Festival f = new Festival();
+                boolean check;
+                String YesNo;
+                YesNo = editIsNFMC.getEditableText().toString();
+
+                //convert isNFMC to boolean answers
+                if (YesNo.equalsIgnoreCase("yes")){
+                    check = true;
+                }
+                else{
+                    check = false;
+                }
+
+                //attach to hashmap and push to database
+                f.name = editFestivalName.getText().toString().trim();
+                f.isNFMC = check;
+                DBManager.Festivals.put(f);
+                Toast toast = Toast.makeText(v.getContext(), "New festival saved", Toast.LENGTH_LONG);
+                toast.show();
+
+                //clear input boxes
+                editFestivalName.setText("");
+                editIsNFMC.setText("");
+            }
+        });
 
     } // End OnCreate
 }
