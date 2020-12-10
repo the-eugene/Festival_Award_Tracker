efb65aefcc7d2656dc674742e789c2d532b86546
CaylaT <72427853+caylaet@users.noreply.github.com>
2020-12-09 22:35:04 -0500

    JavaDoc  and commented the MainEmptyActivity.java

--- app/src/main/java/com/example/festivalawardtracker/MainEmptyActivity.java
+++ app/src/main/java/com/example/festivalawardtracker/MainEmptyActivity.java
@@ -1,56 +1,71 @@
 package com.example.festivalawardtracker;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.example.festivalawardtracker.ui.studentUser.StudentUserFragment;
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.Query;
 
-import java.util.ArrayList;
-import java.util.Map;
+/**
+ * This class is the first thing that is brought up when the app is first opened
+ * it will check to see if the user is currently logged in or not.
+ * If they are logged in it will check to see if they are a student or teacher than
+ * send them to the main activity with their ID and type.
+ * If they are not logged in it will send them to the login/sign up activity.
+ * @author Cayla, Carlos, Jimmy, & Eugene
+ */
 
 public class MainEmptyActivity extends Activity {
 
+    //For debugging purposes
     private final String TAG = "MainEmptyActivity";
+    //For storing the current logged in users email
     String email;
-    FirebaseAuth mAuth;
+    //For initializing an Intent
     Intent activityIntent;
+
+
+    /**
+     * Responsible for checking if a user is signed in. If they are not send them to login/sign up.
+     * If they are signed in  download the database, and retrieve their Id. After the database is
+     * downloaded send them to the main activity.
+     * @author Cayla, Carlos, Jimmy, & Eugene
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Task<GetTokenResult> b = User.getIdToken();
-//        Log.d("Token", String.valueOf(b));
 
+
+        /**
+         * For downloading the database, as well as retrieving IDs and sending user to
+         * the correct UI
+         * @author Cayla, Carlos, Jimmy, & Eugene
+         */
         class queryThread implements Runnable {
             final Activity activity;
 
+            //retrieves activity that called this class
             queryThread(Activity activity) {
                 this.activity = activity;
             }
 
             @Override
             public void run() {
+                //Download all teachers and students
                 Log.d(this.getClass().getName(), "Loading Teacher and Student Database...");
                 DBManager.Teachers.loadAll();
                 DBManager.Students.loadAll();
                 Student student = DBManager.getStudentByEmail(email);
-//                Log.d(TAG,"Student:"+ student.getFirstName());
                 Teacher teacher = DBManager.getTeacherByEmail(email);
-//                Log.d(TAG,"Teacher:" + teacher.getFirstName());
+                //If a student is found get their Id and send them to the main activity, as well as download the rest of the database.
                 if (student != null){
-                    Log.d(TAG,"Found Student");
+                    Log.d(TAG,"Found Student, Student: "+ student.getFirstName());
                     Log.d(this.getClass().getName(), "Loading Festival and Event Database...");
                     DBManager.Festivals.loadAll();
                     DBManager.EventDescriptions.loadAll();
@@ -58,12 +73,14 @@ public class MainEmptyActivity extends Activity {
                     DBManager.SchoolYears.loadAll();
                     DBManager.currentYear = DBManager.findCurrentYear();
                     Log.d(this.getClass().getName(), "...Finished");
-                    Intent activityIntent = new Intent(MainEmptyActivity.this, MainActivity.class);
+                    activityIntent = new Intent(MainEmptyActivity.this, MainActivity.class);
                     activityIntent.putExtra("StudentID",student.ID);
                     activityIntent.putExtra("Type", 2);
                     startActivity(activityIntent);
-                }else if (teacher != null){
-                    Log.d(TAG,"Found Teacher");
+                }
+                //If a teacher is found get their Id and send them to the main activity, as well as download the rest of the database.
+                else if (teacher != null){
+                    Log.d(TAG,"Found Teacher, Teacher: " + teacher.getFirstName());
                     Log.d(this.getClass().getName(), "Loading Festival and Event Database...");
                     DBManager.Festivals.loadAll();
                     DBManager.EventDescriptions.loadAll();
@@ -71,56 +88,31 @@ public class MainEmptyActivity extends Activity {
                     DBManager.SchoolYears.loadAll();
                     DBManager.currentYear = DBManager.findCurrentYear();
                     Log.d(this.getClass().getName(), "...Finished");
-                    Intent activityIntent = new Intent(MainEmptyActivity.this, MainActivity.class);
-                    activityIntent.putExtra("Teacher",teacher.ID);
+                    activityIntent = new Intent(MainEmptyActivity.this, MainActivity.class);
+                    activityIntent.putExtra("TeacherID",teacher.ID);
                     activityIntent.putExtra("Type",1);
                     startActivity(activityIntent);
 
-                }else{
+                }
+                /*If they are not a student or a teacher they shouldn't have been able to login so upon reopening
+                app it should direct them to either a student or a teacher if it reaches this pont something went wrong
+                during the login process or trying to find them in the data base.
+                */
+                else{
                     Log.d(TAG, "something went wrong further up the line");
-//                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//                    AuthUI.getInstance()
-//                            .signOut(MainEmptyActivity.this);
-//                    user.delete()
-//                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                @Override
-//                                public void onComplete(@NonNull Task<Void> task) {
-//                                    if (task.isSuccessful()) {
-//                                        Log.d(TAG, "User account deleted.");
-//                                    }
-//                                }
-//                            });
 
                 }
-
-
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Log.d(this.getClass().getName(), "Loading Festival and Event Database...");
-//                        DBManager.Festivals.loadAll();
-//                        DBManager.EventDescriptions.loadAll();
-//                        DBManager.Events.loadAll();
-//                        DBManager.SchoolYears.loadAll();
-//                        DBManager.currentYear = DBManager.findCurrentYear();
-//                        Log.d(this.getClass().getName(), "...Finished");
-//                    }
-//                });
-
-
             }
         }
 
-        // go straight to main if a token is stored
+        // retrieve who the current user is
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//        AuthUI.getInstance()
-//                .signOut(MainEmptyActivity.this);
+        //If there is a current user retrieve email and start a query
         if (user != null) {
             email = user.getEmail();
             Log.d(TAG, "Current Users email: "+ email);
             new Thread(new queryThread(MainEmptyActivity.this)).start();
-//            activityIntent = new Intent(this, SignInUpActivity.class);
-//            startActivity(activityIntent);
+        //If there isn't a current user send them to the login/Sign up activity
         } else {
             activityIntent = new Intent(this, SignInUpActivity.class);
             startActivity(activityIntent);
