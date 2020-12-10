71b175cc304ad919982c4ab55099e3da8cd81305
the-eugene <72322861+the-eugene@users.noreply.github.com>
2020-12-10 16:39:12 -0600

    JavaDoc Person, Schoolyear, Student, Teacher

--- app/src/main/java/com/example/festivalawardtracker/Person.java
+++ app/src/main/java/com/example/festivalawardtracker/Person.java
@@ -5,7 +5,12 @@ import androidx.annotation.NonNull;
 import java.time.LocalDate;
 import java.time.Period;
 
-public class Person extends DBAware implements Comparable<Person>{
+/**
+ * Model Class representing a person, also serves as a parent class to Student and Teacher
+ * in the future this class can be used for Parents
+ * NFMC needs the full name of a person including the middle name, as well as their birthday to track individuals
+ */
+public class Person extends DBAware implements Comparable<Person> {
     public String firstName;
     public String middleName;
     public String lastName;
@@ -13,24 +18,31 @@ public class Person extends DBAware implements Comparable<Person>{
     public LocalDate birthday;
     public Contact contact;
 
-    public Person(String firstName, String middleName) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-    }
+    /**
+     * No argument constructor for firebase
+     */
+    public Person() {}
 
-    public Person() {
-
-    }
-
+    /**
+     * Adds contact information
+     * @see Contact
+     * @param business address line - business name
+     * @param phone phone number
+     * @param email email address
+     * @param street address line - street address
+     * @param city address - city
+     * @param state address - state, can be a full name of the state or the two digit code
+     * @param zip address - zip code
+     */
     public void setContact(String business, String phone, String email, String street, String city, String state, String zip) {
         setContact(new Contact(business, phone, email, street, city, state, zip));
         //TODO: V2, search db for identical contact (Parents, repeat Event Locations)
-        // What's V2? Carlos
     }
 
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -38,6 +50,7 @@ public class Person extends DBAware implements Comparable<Person>{
     public String getMiddleName() {
         return middleName;
     }
+
     public void setMiddleName(String middleName) {
         this.middleName = middleName;
     }
@@ -45,6 +58,7 @@ public class Person extends DBAware implements Comparable<Person>{
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -52,53 +66,108 @@ public class Person extends DBAware implements Comparable<Person>{
     public Gender getGender() {
         return gender;
     }
-    public void setGender(Gender gender) { this.gender = gender; }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
     public String getBirthday() {
         return birthday.toString();
     }
-    public void setBirthday(String birthday) { this.birthday = LocalDate.parse(birthday);
-    }
-    public Contact getContact() { return contact; }
-    public void setContact(Contact contact) { this.contact = contact; }
 
+    public void setBirthday(String birthday) {
+        this.birthday = LocalDate.parse(birthday);
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    /**
+     * Sets the full name of a person all at once
+     * @param first First Name
+     * @param middle Middle Name
+     * @param last Last Name
+     */
     public void setName(String first, String middle, String last) {
         setFirstName(first);
         setMiddleName(middle);
         setLastName(last);
     }
-    public String getFullName(){
+
+    /**
+     * @return String containing the person's name as First M. Last
+     */
+    public String getFullName() {
         return String.format("%s %s. %s", getFirstName(), getMiddleName().charAt(0), getLastName());
     }
 
-    public String getGenderString(){return getGender().toString();}
-
-    public Integer getAge(){return getAge(LocalDate.now());}
-    public Integer getAge(LocalDate onDate){
-        return (Integer)Period.between(birthday, onDate).getYears();
+    /**
+     * @return a String with person's Gender
+     */
+    public String getGenderString() {
+        return getGender().toString();
     }
 
+    /**
+     * @return int of the person's age right now
+     */
+    public Integer getAge() {
+        return getAge(LocalDate.now());
+    }
+
+    /**
+     * Calculate age at a particular date - important for NFMC
+     * @param onDate The date at which the age will be calculated
+     * @return int representing the person's age on that date
+     */
+    public Integer getAge(LocalDate onDate) {
+        return (Integer) Period.between(birthday, onDate).getYears();
+    }
+
+    /**
+     * Get a short location string
+     * @return formatted string containing city and state code
+     */
     public String shortLocation() {
         return String.format("%s, %s", getContact().city, getContact().stateCode());
     }
-    public String getEmail(){return contact.email;}
-    public static Class<Person> ofType(){return Person.class;}
 
-    @Override
-    public int compareTo(Person o) {
-        return lastName.compareTo(o.lastName);
+    public String getEmail() {
+        return contact.email;
     }
 
+    /**
+     * Allows for sorting of people by their name
+     * @param o Person being compared to
+     * @return comparison between their names: last and first.
+     *
+     */
+    @Override
+    public int compareTo(Person o) {
+        int last = lastName.compareTo(o.lastName);
+        return last!=0?last:firstName.compareTo(o.firstName);
+    }
+
+    /**
+     * Represents a gender
+     */
     public enum Gender {
         MALE,
-        FEMALE
-        ;
+        FEMALE;
 
         @NonNull
         @Override
         public String toString() {
-            if (this==MALE) {return "Male";}
-            else{return "Female";}
+            if (this == MALE) {
+                return "Male";
+            } else {
+                return "Female";
+            }
         }
     }
 }
